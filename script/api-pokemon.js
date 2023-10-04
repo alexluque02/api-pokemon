e24c36fdd4b76a5ef83f162de36b47df6f894708
alexluque02 <alexanderluquehoffrogge@gmail.com>
2023-10-04 16:56:16 +0200

    Función de filtrado hecha correctamente, faltan estilos (más adelante)

--- script/api-pokemon.js
+++ script/api-pokemon.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     var listadoPokemon;
+    var elementosPokemonOcultos = [];
+
     $.ajax({
         type: "GET",
         url: "https://pokeapi.co/api/v2/pokemon?limit=151/",
@@ -10,23 +12,22 @@ $(document).ready(function () {
 
             var nameReplace = pokemon.name.replace(/-/g, "_");
             var template = `
-            <div class="col-lg-3 col-md-6 col-sm-12 mb-3" class="cartaPokemon" id="${pokemon.name}">
-                                <a href=""></a>
-                                <div class="card">
-                                    <img src="https://www.pkparaiso.com/imagenes/xy/sprites/animados/${nameReplace}.gif" style="height:150px; width:110px; text-align:center;"
-                                        class="card-img-top" alt="" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">${pokemon.name}</h5>
-                                        <p class="card-text">
-                                            <li>#${indice}</li>
-                                        </p>
-                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                            data-bs-target="#modalDetails">More details</button>
-
-                                    </div>
-                                </div>
-                                </a>
-                            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-3 cartaPokemon" id="${pokemon.name}">
+                <a href=""></a>
+                <div class="card">
+                    <img src="https://www.pkparaiso.com/imagenes/xy/sprites/animados/${nameReplace}.gif" style="height:150px; width:110px; text-align:center;"
+                        class="card-img-top" alt="" />
+                    <div class="card-body">
+                        <h5 class="card-title">${pokemon.name}</h5>
+                        <p class="card-text">
+                            <li>#${indice}</li>
+                        </p>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#modalDetails">More details</button>
+                    </div>
+                </div>
+                </a>
+            </div>
             `;
             $('#listadoPokemon').append(template);
             indice++;
@@ -45,43 +46,64 @@ $(document).ready(function () {
             habitatList.push(habitat.name);
         });
 
+        var habitatLinks = habitatList.map(function (habitatName) {
+            return `<button type="button" class="btn btn-filter-habitat" habitat="${habitatName}">${habitatName}</button>`;
+        });
+
+        var template = `
+        <div class="habitats col-12 bg-light m-3 d-flex w-100">
+            <button type="button" id="allHabitats" class="btn"><h5>ALL</h5></button>
+            <h5 style="text-transform: uppercase;">${habitatLinks.join("&nbsp; ")}</h5>
+            <button type="button" id="back-btn" class="btn"><i class="bi bi-arrow-left-short"></i></button>
+        </div>
+        `;
+
+
+        $('#listadoHabitats').hide();
+        $('#listadoHabitats').append(template);
+
         $(document).on('click', '#selectHabitats', function () {
             $('#selectHabitats').hide();
+            $('#listadoHabitats').show();
 
-            var habitatLinks = habitatList.map(function (habitatName) {
-                return `<button type="button" class="btn-filter-habitat" habitat="${habitatName}">${habitatName}</button>`;
-            });
+            $(document).on('click', '#allHabitats', function () {
+                elementosPokemonOcultos.forEach(function (elemento) {
+                    elemento.show();
+                });
+            })
 
-            var template = `
-            <div class="habitats col-12 bg-light m-3 d-flex w-100">
-                <h5>ALL</h5>
-                <h5 style="text-transform: uppercase;">${habitatLinks.join("&nbsp; ")}</h5>
-                <i class="bi bi-arrow-left-short"></i>
-            </div>
-            `;
-            $('#listadoHabitats').append(template);
+        })
+
+        $(document).on('click', '#back-btn', function () {
+            $('#listadoHabitats').hide();
+            $('#selectHabitats').show();
         })
     });
 
     $(document).on('click', '.btn-filter-habitat', function () {
         var habitatClicked = $(this).attr("habitat");
+
+        // Mostrar todos los elementos ocultos cuando se filtra un hábitat
+        elementosPokemonOcultos.forEach(function (elemento) {
+            elemento.show();
+        });
+
         $.ajax({
             type: "GET",
             url: `https://pokeapi.co/api/v2/pokemon-habitat/${habitatClicked}`
         }).done(function (resp) {
-            var listadoEspecies = resp.pokemon_species;
-            var listaEspecies = [];
+            var pokemonListInHabitat = resp.pokemon_species.map(pokemon => pokemon.name);
 
+            // Ocultar Pokémon que no pertenecen al hábitat
             listadoPokemon.forEach(pokemon => {
-                if (pokemon.name.includes(habitatClicked)) {
-                    $(`#${pokemon.name}`).hide();
+                if (!pokemonListInHabitat.includes(pokemon.name)) {
+                    var elementoPokemon = $(`#${pokemon.name}`);
+                    elementoPokemon.hide();
+                    elementosPokemonOcultos.push(elementoPokemon);
                 }
-            })
-
-
+            });
         });
     });
-
 });
 
 
