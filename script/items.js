dee98b64819eb0719f3e006edd690af13c399e5f
cpcx04 <pulidocabellochristian@gmail.com>
2023-10-05 20:10:11 +0200

    Almost ready

--- script/items.js
+++ script/items.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
     var listadoItems;
     $.ajax({
         type: "GET",
-        url: "https://pokeapi.co/api/v2/item?limit=2110",
+        url: "https://pokeapi.co/api/v2/item?limit=950",
         success: function (response) {
             listadoItems = response.results;
             listadoItems.forEach(item => {
+                var nameReplace = item.name.replace(/_/g, "-");
                 var template = `
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-3 cartaPokemon" id="${item.name}">
                                 <a href=""></a>
@@ -17,7 +18,7 @@ $(document).ready(function () {
                                         <p class="card-text">
                                             <li>#${getItemIdFromUrl(item.url)}</li>
                                         </p>
-                                        <button type="button" class="btn btn-primary moredetails" pokeid="${getPokemonIdFromUrl(pokemon.url)}">More details</button>
+                                        <button type="button" class="btn btn-primary moredetails" pokeid="${getItemIdFromUrl(item.url)}">More details</button>
 
                                     </div>
                                 </div>
@@ -27,6 +28,19 @@ $(document).ready(function () {
                 $('#listadoItems').append(template)
             });
         }
+    });
+    $(document).on('click', '.moreDetails', function () {
+        var itemId = $(this).attr('pokeid');
+        $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/item/${itemId}`,
+            success: function (response) {
+                var nameReplace = item.name.replace(/_/g, "-");
+                var newSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${nameReplace}.png`
+                $('#imagenItem').attr('src', newSrc);
+                $('#modalDetails').modal('show')
+            }
+        });
     });
     function getItemIdFromUrl(url) {
         // Sacar id de la url de pokemon
