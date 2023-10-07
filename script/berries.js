90dd3aafc0d32ae7a436832f88c2e9f0899d226c
alexluque02 <alexanderluquehoffrogge@gmail.com>
2023-10-07 16:22:24 +0200

    Buscar listo

--- script/berries.js
+++ script/berries.js
@@ -2,40 +2,62 @@ $(document).ready(function () {
     var listadoBerries;
     var campoDeBusqueda = $('#barraBuscar');
     var resultados = 20;
-    var totalPokemon
+    var totalBerries;
     var offset;
-    var urlPokemon = `https://pokeapi.co/api/v2/berry?limit=20&offset=${offset}`;
+    var urlBerries = `https://pokeapi.co/api/v2/berry?limit=20&offset=${offset}`;
     var paginas;
     var pagActual = 0;
+
     $.ajax({
         type: "GET",
-        url: "https://pokeapi.co/api/v2/berry?limit=64/",
-        success: function (response) {
-            listadoBerries = response.results;
-            listadoBerries.forEach(berries => {
-                var nameReplace = berries.name.replace(/-/g, "_");
-                var template = `
-                <div class="col-lg-3 col-md-6 col-sm-12 mb-3 cartaPokemon" id="${berries.name}">
-                                <a href=""></a>
-                                <div class="card">
-                                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${berries.name}-berry.png" style="height:150px; width:110px; text-align:center;"
-                                        class="card-img-top" alt="" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">${berries.name}</h5>
-                                        <p class="card-text">
-                                            <li>#${getBerrieIdFromUrl(berries.url)}</li>
-                                        </p>
-                                        <button type="button" class="btn btn-primary moredetails" pokeid="${getBerrieIdFromUrl(berries.url)}">More details</button>
-
-                                    </div>
-                                </div>
-                                </a>
-                            </div>
-                `
-                $('#listadoBerries').append(template);
-            });
-        }
+        url: urlBerries,
+    }).done(function (resp) {
+        listadoBerries = resp.results;
+        totalBerries = resp.count;
+        mostrarListado(listadoBerries);
     });
+
+    $(document).on('keyup', '#barraBuscar', function () {
+        buscar();
+    });
+
+    function buscar() {
+        valorBusqueda = campoDeBusqueda.val().toLowerCase();
+
+        $('.cartaBerry').each(function () {
+            var nombreBerry = $(this).find('.card-title').text().toLowerCase();
+            if (nombreBerry.includes(valorBusqueda)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
+
+    function mostrarListado(listado) {
+        listado.forEach(berries => {
+            var template = `
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-3 cartaBerry" id="${berries.name}">
+                            <a href=""></a>
+                            <div class="card">
+                                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${berries.name}-berry.png" style="height:150px; width:110px; text-align:center;"
+                                    class="card-img-top" alt="" />
+                                <div class="card-body">
+                                    <h5 class="card-title">${berries.name}</h5>
+                                    <p class="card-text">
+                                        <li>#${getBerrieIdFromUrl(berries.url)}</li>
+                                    </p>
+                                    <button type="button" class="btn btn-primary moredetails" pokeid="${getBerrieIdFromUrl(berries.url)}">More details</button>
+
+                                </div>
+                            </div>
+                            </a>
+                        </div>
+            `
+            $('#listadoBerries').append(template);
+        });
+    }
+
     function getBerrieIdFromUrl(url) {
         // Sacar id de la url de pokemon
         return url.split('/').reverse()[1];
